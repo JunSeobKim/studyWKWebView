c6da1c5e07d2bad4ce84eda7842eb4978edbda14
JunSeobKim <wnstjq616@gmail.com>
2020-11-22 01:10:17 +0900

    WebViewMain

--- studyWKWebView/ViewController.swift
+++ studyWKWebView/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var WebViewMain: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // url string
+        let urlString = "https://google.com"
+        if let url = URL(string: urlString) {
+            let urlReq = URLRequest(url: url)
+            WebViewMain.load(urlReq)
+        }
+        
+        // url > request
+        // req > load
     }
-
 
 }
 
